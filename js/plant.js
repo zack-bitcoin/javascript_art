f8564fed3123f9de0572e0a9ced6184d994830c8
zack-bitcoin <zack.bitcoin@gmail.com>
2019-10-15 12:28:20 -0300

    now the forest is infinite

--- js/plant.js
+++ js/plant.js
@@ -1,8 +1,11 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var perspective = {x:0,z:500,theta:0};
+var controls = {37:false, 38:false, 39:false, 40:false, 65:false, 83:false};
 
-var fps = 20;
+var fps = 50;
+var mapSize = 1600;
+var vision = mapSize - 200;
 var colors = ["#880000",//red
               "#000000",//black
               "#008800",//green
@@ -15,23 +18,72 @@ var colors = ["#880000",//red
              ];
 var pdb = pdb_maker();
 var triangles =
-    grove(pdb, pine, -400, 800, 10, 10, 600, 800, 0, 2, 30).concat(
-        grove(pdb, coral, -400, 800, 10, 10, 600, 800, 0.3, 0.2, 100)).concat(
-            grove(pdb, kelp, -400, 800, 10, 10, 600, 800, 0.1, 0.1, 50)).concat(
-                grove(pdb, daisy, -400, 800, 20, 0, 600, 800, 1, 0.1, 100)).concat(
-                    grove(pdb, redwood, -400, 800, -40, 10, 600, 800, 5, 0, 2));
+    grove(pdb, pine, 0, mapSize, 10, 10, 0, mapSize, 0, 2, 30).concat(
+        grove(pdb, coral, 0, mapSize, 10, 10, 0, mapSize, 0.3, 0.2, 100)).concat(
+            grove(pdb, kelp, 0, mapSize, 10, 10, 0, mapSize, 0.1, 0.1, 50)).concat(
+                grove(pdb, daisy, 0, mapSize, 20, 0, 0, mapSize, 1, 0.1, 100)).concat(
+                    grove(pdb, redwood, 0, mapSize, -40, 10, 0, mapSize, 5, 0, 10));
 
 cron();
 function cron(){
+    movement([37,38,39,40,65,83]);
     var pdb2 = pdb.perspective();
     draw_helper(pdb2, triangles);
     setTimeout(cron, 1000/fps);
+};
+function movement(L){
+    if(L.length == 0){return(0);};
+    var H = L[0];
+    if(controls[H]){
+        keys[H]();
+    };
+    return(movement(L.slice(1)));
+};
+
+function make_3_point(a, b, c) {
+    return {x: a, y: b, z: c};
+}
+function pos_mod(A, B) {
+    return(((A % B) + B) % B);
+}
+function in_perspective(point, rotation) {
+    var X = point.x - perspective.x;
+    var Y = point.y;
+    var Z = point.z - perspective.z;
+    var X = pos_mod(X, mapSize) - (mapSize / 2);
+    var Z = pos_mod(Z, mapSize) - (mapSize / 2);
+    //var X = (X % mapSize) - (mapSize / 2);
+    //var Z = (Z % mapSize) + (mapSize / 2);
+    //var X = X + (1200 * Math.floor(perspective.x / 1200));
+    //var Z = Z + (1200 * Math.floor(perspective.z / 1200));
+    //var X = X + (mapSize * Math.floor((perspective.x) / mapSize));
+    //var Z = Z + perspective.z - 500;
+    //console.log([X, Y]);
+    var point2 = make_3_point(X, Y, Z);
+    var point3 = mul_v_m(point2, rotation);
+    return(point3);
+};
+function mul_v_v(p, v) {
+    return (p.x*v[0]) + (p.y * v[1]) + (p.z * v[2]);
+};
+function mul_v_m(p, m){
+    var p2 = JSON.parse(JSON.stringify(p));
+    p2.x = mul_v_v(p, m[0]);
+    p2.y = mul_v_v(p, m[1]);
+    p2.z = mul_v_v(p, m[2]);
+    return(p2);
+};
+function rotation_matrix_y(angle) {
+    return([
+        [Math.cos(angle),0,Math.sin(angle)],
+        [0,1,0],
+        [-Math.sin(angle),0,Math.cos(angle)]]);
 };
 
 function pdb_maker() {
     var db = {type: "points"};
     var top = 0;
-    var x = {
+    return({
         top: top,
         db: db,
         add: function(x,y,z) {
@@ -48,8 +100,7 @@ function pdb_maker() {
             };
             return(db2);
         }
-    };
-    return(x);
+    });
 };
 
 
@@ -84,7 +135,7 @@ function kelp(db, x, y, z, size) {
     return(make_plant(db, x, y, z, size*20, size*20, 20, 1, colors[5]));
 };
 function redwood(db, x, y, z, size) {
-    return(make_plant(db, x, y, z, size*15, size*20, 20, 4/5, colors[0]));
+    return(make_plant(db, x, y, z, size*15, size*10, 20, 4/5, colors[0]));
 };
 function daisy(db, x, y, z, size) {
     return(make_plant(db, x, y, z, size/3, size*1, 3, 1.7, colors[3]));
@@ -103,32 +154,7 @@ function grove(db, tree, x, x2, y, y2, z, z2, size, size2, many) {
 };
 
 
-function in_perspective(point, rotation) {
-    var point2 = make_3_point(point.x - perspective.x, point.y, point.z - perspective.z);
-    var point3 = mul_v_m(point2, rotation);
-    return(point3);
-};
-function mul_v_v(p, v) {
-    return (p.x*v[0]) + (p.y * v[1]) + (p.z * v[2]);
-};
-function mul_v_m(p, m){
-    var p2 = JSON.parse(JSON.stringify(p));
-    p2.x = mul_v_v(p, m[0]);
-    p2.y = mul_v_v(p, m[1]);
-    p2.z = mul_v_v(p, m[2]);
-    return(p2);
-};
 
-function rotation_matrix_y(angle) {
-    return([
-        [Math.cos(angle),0,Math.sin(angle)],
-        [0,1,0],
-        [-Math.sin(angle),0,Math.cos(angle)]]);
-};
-
-function make_3_point(a, b, c) {
-    return {x: a, y: b, z: c};
-}
 function clockwise(p1, p2, p3) {
     //checking the sign of the cross product of a couple vectors made from these points.
     var v1 = [(p2.x - p1.x), (p2.y - p1.y)];
@@ -153,7 +179,9 @@ function draw_triangles(corners, Tris) {
         var P0 = corners[tri[0]];
         var P1 = corners[tri[1]];
         var P2 = corners[tri[2]];
-        if ((P0.z > 0) && (P1.z > 0) && (P2.z > 0)) {
+        var f = function(Z) {return ((Z.z > 0) && (Z.z < vision) && (Z.x > -(vision/2)) && (Z.x < (vision/2)));};
+        if(f(P0) && f(P1) && f(P2)) {
+            //if ((P0.z > 0) && (P1.z > 0) && (P2.z > 0)) {
             var p1 = three_to_two(P0);
             var p2 = three_to_two(P1);
             var p3 = three_to_two(P2);
@@ -228,8 +256,21 @@ keys[40] = down;
 keys[65] = step_left;
 keys[83] = step_right;
 document.addEventListener('keydown', function(event) {
-    var f = keys[event.keyCode];
-    if(!(f == undefined)){ f(); };
+    var k = event.keyCode;
+    var cv = controls[k];
+    if(cv == false) {
+        controls[k] = true;
+    };
+
+    //var f = keys[event.keyCode];
+    //if(!(f == undefined)){ f(); };
 //    console.log(event.keyCode);
+});
+document.addEventListener('keyup', function(event) {
+    var k = event.keyCode;
+    var cv = controls[k];
+    if(cv == true) {
+        controls[k] = false;
+    };
 });
  
