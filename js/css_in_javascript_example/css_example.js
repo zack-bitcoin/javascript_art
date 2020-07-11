5fe75e61db7787b18d07bcb29d5a95f90e1c2697
zack-bitcoin <zack.bitcoin@gmail.com>
2020-07-11 08:51:20 -0400

    protect the namespace

--- js/css_in_javascript_example/css_example.js
+++ js/css_in_javascript_example/css_example.js
@@ -1,45 +1,46 @@
-
 //<div style="line-height:24%;">
 
-function br() {
-    return document.createElement("br");
-};
-function div() {
-    return document.createElement("div");
-};
+(function(){
+    function br() {
+        return document.createElement("br");
+    };
+    function div() {
+        return document.createElement("div");
+    };
 
-var short_div = div();
-short_div.style = "line-height:24%;";
-var long_div = div();
-long_div.style = "line-height:124%;";
+    var short_div = div();
+    short_div.style = "line-height:24%;";
+    var long_div = div();
+    long_div.style = "line-height:124%;";
 
-function add_string(div, S) {
+    function add_string(div, S) {
     //// if you use spans to name the text, then the text can be edited later without having to rewrite the entire div.
     //var span1 = document.createElement("span");
     //span1.innerHTML = S;
     //div.appendChild(span1);
 
     //// this makes it identical to the html example.
-    div.insertAdjacentHTML('beforeend', S);
-};
-function add_letters(){
-    add_string(short_div, "abc def ghi");
-};
-function add_numbers(){
-    add_string(long_div, "123 456 789");
-};
+        div.insertAdjacentHTML('beforeend', S);
+    };
+    function add_letters(){
+        add_string(short_div, "abc def ghi");
+    };
+    function add_numbers(){
+        add_string(long_div, "123 456 789");
+    };
 
-add_letters();
-short_div.appendChild(br());
-add_letters();
-short_div.appendChild(br());
-add_letters();
+    add_letters();
+    short_div.appendChild(br());
+    add_letters();
+    short_div.appendChild(br());
+    add_letters();
 
-add_numbers();
-long_div.appendChild(br());
-add_numbers();
-long_div.appendChild(br());
-add_numbers();
+    add_numbers();
+    long_div.appendChild(br());
+    add_numbers();
+    long_div.appendChild(br());
+    add_numbers();
 
-document.body.appendChild(short_div);
-document.body.appendChild(long_div);
+    document.body.appendChild(short_div);
+    document.body.appendChild(long_div);
+})();
