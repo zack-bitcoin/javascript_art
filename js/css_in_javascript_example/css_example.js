97dab155324dbcb2200638bd9a08a465102d0bbc
zack-bitcoin <zack.bitcoin@gmail.com>
2020-07-11 08:36:24 -0400

    organized the css example for more readability

--- js/css_in_javascript_example/css_example.js
+++ js/css_in_javascript_example/css_example.js
@@ -7,34 +7,33 @@ function br() {
 function div() {
     return document.createElement("div");
 };
-function span(){
-    return document.createElement("span");
-};
 
 var short_div = div();
 short_div.style = "line-height:24%;";
+var long_div = div();
+long_div.style = "line-height:124%;";
 
 function add_string(div, S) {
-    var span1 = span();
-    span1.innerHTML = S;
-    div.appendChild(span1);
+    //// if you use spans this way, then the text can be edited later without having to rewrite the entire div.
+    //var span1 = document.createElement("span");
+    //span1.innerHTML = S;
+    //div.appendChild(span1);
+
+    //// this makes it identical to the html example.
+    div.insertAdjacentHTML('beforeend', S);
 };
 function add_letters(){
     add_string(short_div, "abc def ghi");
 };
-
-add_letters();
-short_div.appendChild(br());
-add_letters();
-short_div.appendChild(br());
-add_letters();
-
-var long_div = div();
-long_div.style = "line-height:124%;";
-
 function add_numbers(){
     add_string(long_div, "123 456 789");
 };
+
+add_letters();
+short_div.appendChild(br());
+add_letters();
+short_div.appendChild(br());
+add_letters();
 
 add_numbers();
 long_div.appendChild(br());
