3fbfe6655ba92932c0c3278da9058ee80f8034aa
zack <zack.bitcoin@gmail.com>
2021-03-11 12:05:33 -0300

    seximal number tools

---
+++ js/clock.js
@@ -0,0 +1,121 @@
+//kilometros / hora
+//30 kilometros en 3 horas
+
+
+var clock = (function(){
+    var div = document.createElement("div");
+    document.body.appendChild(div);
+
+    var clock = document.createElement("div");
+    document.body.appendChild(clock);
+
+    cron();
+
+    function cron(){
+        var t = time_now();
+        var spans = t.match(/\d*\./)[0].slice(0,-1);
+        var words = "";
+        if (spans.length > 3){
+            words = two_digits_to_word(spans.slice(0,-2))
+                .concat(" lapse ");
+            spans = spans.slice(2);
+        };
+        if (spans.length > 2){
+            words = two_digits_to_word(spans[0])
+                .concat(" lapse ");
+            spans = spans.slice(1);
+        };
+        if (spans.length > 1){
+            words = words
+                .concat(two_digits_to_word(spans))
+            //.concat(" spans ");
+                .concat(" lull ");
+        };
+        words = words
+            .concat(two_digits_to_word(t.match(/\.\d\d/)[0].slice(1))
+                    .concat(" moments"))
+
+        clock.innerHTML = "<h2>"
+            .concat(t)
+            .concat("</h2>")
+            .concat("<h4>")
+            .concat(words)
+            .concat("</h4>");
+        setTimeout(cron, 100);
+    };
+
+    function two_digits_to_word(d){
+        if(d === "0"){
+            return("zero");
+        } else if(d === "1"){
+            return("one");
+        } else if(d === "2"){
+            return("two");
+        } else if(d === "3"){
+            return("three");
+        } else if(d === "4"){
+            return("four");
+        } else if(d === "5"){
+            return("five");
+        } else if(d === "10"){
+            return("six");
+        } else if(d === "11"){
+            return("seven");
+        } else if(d === "12"){
+            return("eight");
+        } else if(d === "13"){
+            return("nine");
+        } else if(d === "14"){
+            return("ten");
+        } else if(d === "15"){
+            return("eleven");
+        } else if(d === "20"){
+            return("dozen");
+        } else {
+            var dozens = d[0];
+            var ones = d[1];
+            var words = "";
+            if(dozens === "2"){
+                words = words
+                    .concat("dozen");
+            } else if (dozens === "3"){
+                words = words
+                    .concat("thirsy");
+            } else if (dozens === "4"){
+                words = words
+                    .concat("foursy");
+            } else if (dozens === "5"){
+                words = words
+                    .concat("fifsy");
+            };
+            if(!(dozens === "0")){
+                words = words.concat(" ");
+            };
+            if(!(ones === "0")){
+                words = words
+                    .concat(two_digits_to_word(ones));
+            };
+            return(words);
+        };
+    };
+
+    function time_now(){
+        var d = new Date();
+        var hours = d.getUTCHours();
+        var minutes = d.getUTCMinutes();
+        var seconds = d.getUTCSeconds();
+        var milis = d.getUTCMilliseconds();
+        var seximal_day =
+            (((((((milis/1000) +
+                  seconds) / 60) +
+                minutes) / 60) +
+              hours) / 24);
+        var seximal_spans =
+            seximal_day * 216 * 6;
+        var time = seximal_spans.toString(6)
+            .match(/[^.]*.\d\d\d/)[0];
+        return(time);
+    };
+
+    return({ time_now: time_now});
+})();
---
+++ js/time_conversion.js
@@ -0,0 +1,89 @@
+(function(){
+    var div = document.createElement("div");
+    document.body.appendChild(div);
+
+    var hour_label = document.createElement("span");
+    hour_label.innerHTML = "hour";
+    div.appendChild(hour_label);
+    var select_hour = document.createElement("select");
+    add_options(select_hour, 0, 23);
+    div.appendChild(select_hour);
+    //div.appendChild(document.createElement("br"));
+
+    var minute_label = document.createElement("span");
+    minute_label.innerHTML = "minute";
+    div.appendChild(minute_label);
+    var select_minute = document.createElement("select");
+    add_options(select_minute, 0, 59);
+    div.appendChild(select_minute);
+    div.appendChild(document.createElement("br"));
+
+    var convert = document.createElement("input");
+    convert.type = "button";
+    convert.value = "convert to seximal";
+    convert.onclick = convertTime;
+    div.appendChild(convert);
+    div.appendChild(document.createElement("br"));
+    var seximal_result = document.createElement("div");
+    div.appendChild(seximal_result);
+    div.appendChild(document.createElement("br"));
+    div.appendChild(document.createElement("br"));
+
+
+
+    var to_decimal = document.createElement("input");
+    to_decimal.type = "text";
+    div.appendChild(to_decimal);
+    var to_decimal_button = document.createElement("input");
+    to_decimal_button.type = "button";
+    to_decimal_button.value = "convert to decimal";
+    to_decimal_button.onclick = convertToDecimal;
+    div.appendChild(to_decimal_button);
+    var decimal_result = document.createElement("div");
+    div.appendChild(decimal_result);
+    div.appendChild(document.createElement("br"));
+    div.appendChild(document.createElement("br"));
+
+    function convertToDecimal(){
+        var v = parseInt(to_decimal.value, 6);
+        var day = v / (6*6*6*6);
+        var now = new Date();
+        var off = now.getTimezoneOffset();
+        day = day - (off / (24*60));
+        var hours = day * 24;
+        var hours2 = Math.floor(hours);
+        var minutes = Math.round(60*(hours - hours2));
+        div.appendChild(document.createElement("br"));
+        decimal_result.innerHTML = hours2.toString()
+            .concat(":")
+            .concat(minutes);
+    };
+
+    function convertTime(){
+        var now = new Date();
+        var off = now.getTimezoneOffset();
+
+        var m = parseInt(select_minute.value, 10);
+        var h = parseInt(select_hour.value, 10);
+        var t = (h*60) + m + off;
+        if(t < 0){
+            t = t + (24*60);
+        };
+        seximal_day = t / (24*60);
+        var seximal_spans =
+            seximal_day * 216 * 6;
+        var time = seximal_spans.toString(6)
+            .match(/[^.]*.?\d?\d?\d?/)[0];
+        seximal_result.innerHTML = time;
+        //return(time);
+    };
+    
+    function add_options(select, start, end){
+        for(var i = start; i<end; i++){
+            var option = document.createElement("option");
+            option.innerHTML = i;
+            option.value = i;
+            select.appendChild(option);
+        };
+    };
+})();
